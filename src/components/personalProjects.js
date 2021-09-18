d2ede43aac634214e23f89ecbf01901ad147825f
Vinam Tuteja <deepvinam@hotmail.com>
2021-09-18 09:24:48 -0700

    first commit

--- src/components/personalProjects.js
+++ src/components/personalProjects.js
@@ -1,5 +1,6 @@
 import linkedinClone from '../images/linkedin-clone.png'
 import vint from '../images/vint.png'
+import mynotes from '../images/mynotes.png'
 
 
 const personalProjects = [
@@ -20,11 +21,23 @@ const personalProjects = [
       name: "Portfolio Website",
       hyperlink: "#",
       target:"",
-      gitlink:"https://github.com/Vinam09/linkedin-clone",
+      gitlink:"https://github.com/Vinam09/VIN-T",
       skills: "React, HTML, JavaScript, CSS, Tailwind CSS",
       description:
         "My portfolio website built with React.",
     },
+    {
+      id: 2,
+      picture: mynotes,
+      name: "MyNotes",
+      hyperlink: "https://mynotes-106d7.web.app/",
+      target:"_blank",
+      gitlink:"https://github.com/Vinam09/my-notes",
+      skills: "React, Redux, Firebase, HTML, JavaScript, CSS, Tailwind CSS",
+      description:
+        "Chrome extension app for taking quick notes while browsing the web linked with firebase making it easy to store notes and revisit them later",
+    },
+
   ];
   
   export default personalProjects;
